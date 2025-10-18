9043f93a6bbe489ccf6ce0237ebe9537c0477216
rudimah <hamidur.rahman@outlook.fr hamidur>
2025-10-18 12:05:16 +0200

    use extern db

--- back/models/database.py
+++ back/models/database.py
@@ -7,5 +7,6 @@ def get_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
